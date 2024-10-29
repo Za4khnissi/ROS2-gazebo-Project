2510cdd09c36c93fdc2d28007091f93088d667a4
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2024-10-29 04:43:50 -0400

    suppression dossier temporaires

--- build/ros_gz_example_application/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zak/inf3995/project_ws/src/ros_gz_example_application
-# Build directory: /home/zak/inf3995/build/ros_gz_example_application
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/voice_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/zak/inf3995/install/voice_control'
