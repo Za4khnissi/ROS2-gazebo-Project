5027dd2d2b90f358ceab344cde28fa5a7df41c44
Jodel <marcjodel.a@gmail.com>
2024-11-05 22:45:20 -0500

    Merge battery status with old Jo

---
+++ client/src/app/pages/battery-status/battery-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-battery-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './battery-status.component.html',
+  styleUrl: './battery-status.component.css'
+})
+export class BatteryStatusComponent {
+
+}
