464bf77f6e26a094a62e86044eba25b688e6890d
Jodel <marcjodel.a@gmail.com>
2024-11-30 22:16:57 -0500

    V3 Help added for heuristique

--- client/src/app/pages/choose-mode/choose-mode.component.ts
+++ client/src/app/pages/choose-mode/choose-mode.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-choose-mode',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './choose-mode.component.html',
   styleUrl: './choose-mode.component.css'
 })
 export class ChooseModeComponent {
   constructor(private router: Router) {}
+  isHelpVisible = false;
 
   goToPhysicalRobot() {
     this.router.navigate(['/robot/physical'])
@@ -22,6 +24,10 @@ export class ChooseModeComponent {
   goToHistory()
   {
     this.router.navigate(['/robot/history']);
+  }
+
+  toggleHelp() {
+    this.isHelpVisible = !this.isHelpVisible;
   }
 
 
